af2f7b76d5c9e8da1557daa0ac3ac9f6e7fc4d4c
lalit <lalitvijay9480@gmail.com>
2025-04-20 01:40:55 +0530

    fix prod backend url

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8787"
-      : "backend.lalit2005.workers.dev/api/v1",
+      : "https://backend.lalit2005.workers.dev/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
